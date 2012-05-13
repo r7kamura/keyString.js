6a1a4008ea97a3aba6f310b332c3e93a9b080d57
Ryo NAKAMURA <r7kamura@gmail.com>
2012-05-13 22:30:01 +0900

    change code map and add indent

--- keyString.js
+++ keyString.js
@@ -5,9 +5,9 @@ function keyString (e) {
   if (e.metaKey && !e.ctrlKey) ret += 'W-';
   if (e.which == 0) {
     if (e.shiftKey) ret += 'S-';
-    ret += arguments.callee.table1[e.keyCode];
+    ret += arguments.callee.codeMap[e.keyCode];
   } else {
-    var key = arguments.callee.table2[e.which];
+    var key = arguments.callee.codeMap[e.which];
     if (key) {
       if (e.shiftKey) ret += 'S-';
       ret += key;
@@ -17,8 +17,36 @@ function keyString (e) {
   }
   return ret;
 }
-keyString.table1 = { 9 : "TAB", 27 : "ESC", 33 : "PageUp", 34 : "PageDown", 35 : "End", 36 : "Home", 37 : "Left", 38 : "Up", 39 : "Right", 40 : "Down", 45 : "Insert", 46 : "Delete", 112 : "F1", 113 : "F2", 114 : "F3", 115 : "F4", 116 : "F5", 117 : "F6", 118 : "F7", 119 : "F8", 120 : "F9", 121 : "F10", 122 : "F11", 123 : "F12" };
-keyString.table2 = { 8 : "BS", 13 : "RET", 32 : "SPC" };
+
+keyString.codeMap = {
+    8: 'BackSpace',
+    9: 'Tab',
+   13: 'Return',
+   27: 'Esc',
+   32: 'Space',
+   33: 'PageUp',
+   34: 'PageDown',
+   35: 'End',
+   36: 'Home',
+   37: 'Left',
+   38: 'Up',
+   39: 'Right',
+   40: 'Down',
+   45: 'Insert',
+   46: 'Delete',
+  112: 'F1',
+  113: 'F2',
+  114: 'F3',
+  115: 'F4',
+  116: 'F5',
+  117: 'F6',
+  118: 'F7',
+  119: 'F8',
+  120: 'F9',
+  121: 'F10',
+  122: 'F11',
+  123: 'F12'
+};
 
 /* Usage (with jQuery):
 *
